8692524c78e693aae30f6b43940eb935e2542e45
Saran <57593599+saran-mani@users.noreply.github.com>
2025-03-17 10:28:17 +0530

    update

--- src/validators/admin/admin.announcement.validator.js
+++ src/validators/admin/admin.announcement.validator.js
@@ -5,13 +5,13 @@ export const addAnnouncementValidator = [
     .isString()
     .notEmpty()
     .withMessage("Title is required and must be a string")
-    .isLength({ min: 3, max: 100 })
+    .isLength({ min: 1, max: 100 })
     .withMessage("Title must be between 3 and 100 characters"),
 
   body("content")
     .isString()
     .notEmpty()
     .withMessage("Content is required and must be a string")
-    .isLength({ min: 10 })
+    .isLength({ min: 1 })
     .withMessage("Content must be at least 10 characters long"),
 ];
