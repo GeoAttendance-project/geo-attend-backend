88531346346ec0238abecd7ca25f1d58cdc08272
Saran <57593599+saran-mani@users.noreply.github.com>
2025-04-05 14:51:58 +0530

    Update studentAttendanceController.js

--- src/controllers/student/studentAttendanceController.js
+++ src/controllers/student/studentAttendanceController.js
@@ -55,7 +55,7 @@ export const markAttendance = catchAsync(async (req, res, next) => {
     );
   }
 
-  const maxDistance = 10;
+  const maxDistance = 7;
 
   const nearbyPoint = await AttendanceLocation.aggregate([
     {
@@ -147,7 +147,7 @@ export const checkTodayAttendance = catchAsync(async (req, res, next) => {
     isAfternoonTime,
     attendanceLocationLatitude,
     attendanceLocationLongitude,
-    radius: 10,
+    radius: 7,
   });
 });
 
